ea9c8bc8c027a6b2ab8c60402ec8dd10c129963f
Ankushjare <jareankush@gmail.com>
2021-10-25 01:42:41 +0530

    Fuction Logic Problems

---
+++ Problem5/Factor/Fatorial_number/Fatorial_number/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+public class Exercise15
+{
+    public static void Main()
+    {
+        /*int i, f = 1, num;
+
+        Console.Write("\n\n");
+        Console.Write("Calculate the factorial of a given number:\n");
+        Console.Write("--------------------------------------------");
+        Console.Write("\n\n");
+
+        Console.Write("Input the number : ");
+        num = Convert.ToInt32(Console.ReadLine());
+        for (i = 1; i <= num; i--)
+            f = f * i;
+
+
+        Console.Write("The Factorial of {0} is: {1}\n", i, f);*/
+        int i, j, num, isPrime;
+        Console.Write("Input the number : ");
+        num = Convert.ToInt32(Console.ReadLine());
+        for (i = 2; i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = 1;
+                for (j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = 0;
+                        break;
+                    }
+                }
+                if (isPrime == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
---
+++ Problem7/Swap_num/Swap_No/Swap_No/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class SwapExample
+{
+    public static void Main(string[] args)
+    {
+        int a = 22, b = 23;
+        Console.WriteLine("Before swap a= " + a + " b= " + b);
+        a = a + b;       
+        b = a - b; 
+        a = a - b; 
+        Console.Write("After swap a= " + a + " b= " + b);
+    }
+}
